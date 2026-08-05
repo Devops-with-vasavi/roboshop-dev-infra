85d0812060471d7f7b62d912a072dd78d5d8f4ce
Vasavi <jellavasavi00@example.com>
2026-08-05 12:30:59 +0530

    msg

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,6 +9,6 @@ touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
 git clone https://github.com/Devops-with-vasavi/Roboshop-Ansible-v3-project.git
-cd roboshop-ansible-v3
+cd Roboshop-Ansible-v3-project
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
