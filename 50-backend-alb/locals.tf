e79744982cc052c28ee40d81d5e24824725a6eb7
Vasavi <jellavasavi00@example.com>
2026-09-10 20:04:29 +0530

    msg

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
     common_name = "${var.project}-${var.environment}"
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] #private_subnet-1a
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #private_subnet-1a
     common_tags = {
         Project = var.project
         environment = var.environment
