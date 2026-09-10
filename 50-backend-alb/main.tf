089661e5c516f8e6e0d285b0c984dad84b6fc222
Vasavi <jellavasavi00@example.com>
2026-09-10 21:00:20 +0530

    msg

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -38,7 +38,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_route53_record" "alb"{
   zone_id = var.zone_id
-  name    = "*.backend-alb-${var.environment}-mydaws90.online" # 
+  name    = "*.backend-alb-${var.environment}.mydaws.online"
   type    = "A"
 
   alias {
