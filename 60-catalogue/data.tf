2674cf5b32ab3ae62da638b72e973fd421553c0d
Vasavi <jellavasavi00@example.com>
2026-09-15 11:25:45 +0530

    msg

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -11,9 +11,6 @@ data "aws_ssm_parameter" "vpc_id" {
     name = "/${var.project}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-}
 
 data "aws_ami" "joindevops" {
   most_recent      = true
